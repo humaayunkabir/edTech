d14869da003d0ce1d8ef355226904498feebc168
Humayun Kabir <kabirhumayun5902@gmail.com>
2022-09-16 17:00:36 +0600

    carousel-caption centered

---
+++ new.js
@@ -0,0 +1,142 @@
+// var fruits = ['Apple', 'Banana', 'Orange'];
+// // var positionIndex = fruits.indexOf('Banana');
+// fruits.pop();
+// fruits.push('Mango');
+// console.log(fruits);
+/*var janeNumber = 95;
+var myNumber = 85;
+var tomNumber = 66;
+var myNumber = 56;
+
+
+if (janeNumber < myNumber) {
+    console.log('ami besi paici');
+}
+else {
+    console.log('A grade');
+}*/
+// for (var i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+// var num = 0;
+// while (num < 10) {
+//     console.log('Out put done');
+//     num++;
+// }
+// function movie() {
+//     return "Din-The day";
+//     return "Poran";
+//     return "Hawa";
+// }
+// console.log(movie());
+// var shopingChart = {
+//     bag: 200,
+//     pen: 5,
+//     book: 120
+// }
+// const keys = Object.keys(shopingChart);
+// console.log(keys);
+
+// const values = Object.values(shopingChart);
+// console.log(values);
+// function isLeapYear(year) {
+//     var lep = year % 4;
+//     if (lep === 0) {
+//         return true;
+//     }
+//     return false;
+// }
+// var isMyLeapYear = isLeapYear(2021);
+// console.log(isMyLeapYear);
+
+// function print(a, b, c) {
+//     return a + 2;
+//     return a * b;
+//     return b * c + a;
+// }
+// console.log(print(1, 2, 3));
+// console.log(Math.min(6, 56, 5, 8, 2));
+
+
+
+// Small man ber korar jnno function
+/*
+function minInArray(numbers) {
+    let smallest = numbers[0];
+    for (let i = 0; i < numbers.length; i++) {
+        const index = i;
+        const element = numbers[index];
+        if (element < smallest) {
+            smallest = element;
+        }
+    }
+    return smallest;
+}
+const small = [77, 76237, 29, 7, -10, -376426, 4573895749];
+const short = minInArray(small);
+console.log(short);
+*/
+
+/*
+function radianToDegree() {
+
+}
+function isJavaScriptFile() {
+
+}
+function oilPrice() {
+
+}
+function publicBusFare() {
+
+}
+function isBestFriend() {
+
+}*/
+/*function radianToDegree(number) {
+    let degree = 57.2958;
+    for (let i = 1; i <= number; i++) {
+        degree = degree * i;
+    }
+    return degree;
+}
+const degree = radianToDegree(6);
+console.log(degree);*/
+
+
+
+
+
+
+// OilPrice
+/*const shopingCart = [
+    { diesel: 114, quantity: 2 },
+    { diesel: 130, quantity: 5 },
+    { diesel: 135, quantity: 3 }
+];
+function totalCost(products) {
+    let sum = 0;
+    for (let i = 0; i < products.length; i++) {
+        const product = products[i];
+        const productTotal = product.diesel * product.quantity;
+        sum = sum + productTotal;
+    }
+    return sum;
+}
+const expense = totalCost(shopingCart);
+console.log(expense);*/
+
+
+// // problem 1
+// function radianToDegree(radian) {
+//     const radianDegreeConvart = radian * (180 / 3.1416);
+//     return radianDegreeConvart.toFixed(2)
+// }
+
+
+// // problem 1
+// function radianToDegree(radian) {
+//     const radianDegreeConvart = radian * (180 / 3.1416);
+//     return radianDegreeConvart.toFixed(2)
+// }
+
